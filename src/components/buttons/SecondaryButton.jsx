df7a86229c64a19482e9922743be4445af679ae4
Joeward Peralta <joeward_peralta@outlook.com>
2025-07-01 19:31:22 -0400

    Change the text color of the Secondary button component to primary

--- src/components/buttons/SecondaryButton.jsx
+++ src/components/buttons/SecondaryButton.jsx
@@ -4,7 +4,8 @@ export default function SecondaryButton({ children, onClick }) {
   return (
     <Button
       onClick={onClick}
-      className="border-[2px] border-primary hover:bg-primary hover:text-white duration-[400ms] cursor-pointer"
+      className="border-[2px] border-primary hover:bg-primary 
+      hover:text-white duration-[400ms] cursor-pointer text-primary"
       size="md"
     >
       {children}
